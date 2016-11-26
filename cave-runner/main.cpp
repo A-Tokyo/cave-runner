767d56d7773f82be6b199d349e0623ba5a09f5d3
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-27 00:54:18 +0200

    add xCode project

--- cave-runner/main.cpp
+++ cave-runner/main.cpp
@@ -1,12 +1,159 @@
-//
-//  main.cpp
-//  cave-runner
-//
-//  Created by Ahmed Mohamed on 11/27/16.
-//  Copyright © 2016 Ahmed Mohamed. All rights reserved.
-//
-
+#include <OpenGl/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
 #include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+#define PI 3.14159265
+#define toDeg 57.2957795131
+#define toRad 0.01745329251
+
+// Global Data Structures
+typedef struct vector {
+    float x;
+    float y;
+    float z;
+    vector(float x, float y, float z) {
+        this->x = x;
+        this->y = y;
+        this->z =z;
+    }
+    vector() {
+        this->x = 0;
+        this->y = 0;
+        this->z =0;
+    }
+    std::string toString() {
+        return "["+ std::to_string(x) + ", "+ std::to_string(y) + ", "+std::to_string(z)+"]" ;
+    }
+    vector unitVector() {
+        float magnitude = sqrtf(pow(x,2)+pow(y,2)+pow(z,2));
+        vector unitVector(x/magnitude,y/magnitude,z/magnitude);
+        return unitVector;
+    }
+    void set(float newx, float newy, float newz) {
+        x = newx;
+        y = newy;
+        z = newz;
+    }
+    void set(vector* vector) {
+        x = vector->x;
+        y = vector->y;
+        z = vector->z;
+    }
+    float magnitude() {
+        return sqrtf(pow(x,2)+pow(y,2)+pow(z,2));
+    }
+} vector;
+typedef struct quadraple {
+    float a;
+    float x;
+    float y;
+    float z;
+    quadraple(float a, float x, float y, float z) {
+        this-> a = a;
+        this->x = x;
+        this->y = y;
+        this->z =z;
+    }
+    quadraple() {
+        this-> a = 0;
+        this->x = 0;
+        this->y = 0;
+        this->z =0;
+    }
+    void set(float a, float x, float y, float z) {
+        this-> a = a;
+        this->x = x;
+        this->y = y;
+        this->z =z;
+    }
+    void set(quadraple* quadraple) {
+        this-> a = quadraple->a;
+        this->x = quadraple->x;
+        this->y = quadraple->y;
+        this->z = quadraple->z;
+    }
+    std::string toString() {
+        return "["+ std::to_string(a) + ", "+ std::to_string(x) + ", "+ std::to_string(y) + std::to_string(z)+"]" ;
+    }
+} quadraple;
+typedef struct gameCamera {
+    double eyeX;
+    double eyeY;
+    double eyeZ;
+    double centerX;
+    double centerY;
+    double centerZ;
+    double upX;
+    double upY;
+    double upZ;
+    vector DefaultEye;
+    vector DefaultCenter;
+    vector DefaultUp;
+    gameCamera(double eyeX, double eyeY, double eyeZ, double centerX, double centerY, double centerZ, double upX, double upY, double upZ) {
+        this->eyeX = eyeX;
+        this->eyeY = eyeY;
+        this->eyeZ = eyeZ;
+        this->centerX = centerX;
+        this->centerY = centerY;
+        this->centerZ = centerZ;
+        this->upX = upX;
+        this->upY = upY;
+        this->upZ = upZ;
+        vector DefaultEye(eyeX,eyeY,eyeZ);
+        vector DefaultCenter(centerX,centerY,centerZ);
+        vector DefaultUp(upX,upY,upZ);
+        this->DefaultEye = DefaultEye;
+        this->DefaultCenter = DefaultCenter;
+        this->DefaultUp = DefaultUp;
+    }
+    void incrementEye(double eyeX, double eyeY, double eyeZ) {
+        this->eyeX += eyeX;
+        this->eyeY += eyeY;
+        this->eyeZ += eyeZ;
+    }
+    void setEye(double eyeX, double eyeY, double eyeZ) {
+        this->eyeX = eyeX;
+        this->eyeY = eyeY;
+        this->eyeZ = eyeZ;
+    }
+    void setCenter(double centerX, double centerY, double centerZ) {
+        this->centerX = centerX;
+        this->centerY = centerY;
+        this->centerZ = centerZ;
+    }
+    void set(double eyeX, double eyeY, double eyeZ, double centerX, double centerY, double centerZ, double upX, double upY, double upZ) {
+        this->eyeX = eyeX;
+        this->eyeY = eyeY;
+        this->eyeZ = eyeZ;
+        this->centerX = centerX;
+        this->centerY = centerY;
+        this->centerZ = centerZ;
+        this->upX = upX;
+        this->upY = upY;
+        this->upZ = upZ;
+    }
+    void reset(){
+        this->eyeX = DefaultEye.x;
+        this->eyeY = DefaultEye.y;
+        this->eyeZ = DefaultEye.z;
+        this->centerX = DefaultCenter.x;
+        this->centerY = DefaultCenter.y;
+        this->centerZ = DefaultCenter.z;
+        this->upX = DefaultUp.x;
+        this->upY = DefaultUp.y;
+        this->upZ = DefaultUp.z;
+    }
+    std::string eyeToString() {
+        return "[" + std::to_string(eyeX) + ", " + std::to_string(eyeY) + ", " + std::to_string(eyeZ) + "]" ;
+    }
+} gameCamera;
+
+
+static int windowHeight = 720;
+static int windowWidth = 1080;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
