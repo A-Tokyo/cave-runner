48669bf41cb3d4cd2e3453385e0a667042f0bf82
Tokyo <ahmed.tokyo1@gmail.com>
2016-12-03 22:39:47 +0200

    updated project structure with gameDrawing header and src

---
+++ cave-runner/gameDrawing.cpp
@@ -0,0 +1,9 @@
+//
+//  gameDrawing.cpp
+//  cave-runner
+//
+//  Created by Ahmed Mohamed on 12/3/16.
+//  Copyright © 2016 Ahmed Mohamed. All rights reserved.
+//
+
+#include "gameDrawing.hpp"
---
+++ cave-runner/gameDrawing.hpp
@@ -0,0 +1,21 @@
+//
+//  gameDrawing.hpp
+//  cave-runner
+//
+//  Created by Ahmed Mohamed on 12/3/16.
+//  Copyright © 2016 Ahmed Mohamed. All rights reserved.
+//
+
+#ifndef gameDrawing_hpp
+#define gameDrawing_hpp
+
+#include <OpenGl/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+// Custom Includes
+#include "gameStructs.hpp"
+
+#endif /* gameDrawing_hpp */
